b9b559af165ecf214e4872df881d5435b7967c72
devoil <devdevoildev@gmail.com>
2025-02-18 21:55:18 +0900

    ✨ Feat: [c-basic] 4. 반복문 - while / do while / for

---
+++ c-basic/4_반복문.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+void main() 
+{
+	// while 문
+	//int i = 1;
+
+	//while (i < 5)
+	//{
+	//	printf("Hello World!");
+	//	i++;
+	//}
+
+	/*while (i <= 9) 
+	{
+		printf("2 * %d = %d\n", i,  2 * i);
+		i++;
+	}*/
+
+	// ===========================================================================
+
+	// do while 문
+	
+	//int sum = 0;
+	//int input = 0;
+
+	// do {
+	//	 printf("정수를 입력하세요 : ");
+	//	 scanf_s("%d", &input);
+	//	 sum += input;
+	// }  while (input != 0);
+
+	// while로 하는 방법
+
+	//printf("정수를 입력하세요 : ");
+	//scanf_s("%d", &input);
+
+	//while (input != 0) { // 만약 상단에 받아주는 값이 없다면 바로 총 합은 0으로 끝
+	//	printf("정수를 입력하세요 : ");
+	//	scanf_s("%d", &input);
+	//	sum += input;
+	//}
+
+	//printf("총 합은 %d", sum);
+
+	// ===========================================================================
+	// for문
+
+	int dan = 0;
+	
+	printf("단을 입력하세요 : ");
+	scanf_s("%d", &dan);
+
+	for (int i = 1; i <= 9; i++)
+	{
+		printf("%d * %d = %d\n", dan, i, dan * i);
+	}
+}
