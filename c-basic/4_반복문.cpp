2740f9fc9b3051d7325d3708701f4c0cc4402123
dev-oil <devdevoildev@gmail.com>
2025-02-19 14:11:54 +0900

    ✨ Feat: [c-basic] 4. 반복문 - 중첩 반복문

--- c-basic/4_반복문.cpp
+++ c-basic/4_반복문.cpp
@@ -46,13 +46,51 @@ void main()
 	// ===========================================================================
 	// for문
 
-	int dan = 0;
-	
-	printf("단을 입력하세요 : ");
-	scanf_s("%d", &dan);
+	//int dan = 0;
+	//
+	//printf("단을 입력하세요 : ");
+	//scanf_s("%d", &dan);
 
-	for (int i = 1; i <= 9; i++)
-	{
-		printf("%d * %d = %d\n", dan, i, dan * i);
+	//for (int i = 1; i <= 9; i++)
+	//{
+	//	printf("%d * %d = %d\n", dan, i, dan * i);
+	//}
+
+	// break 문
+	//int a = 0;
+
+	//while(true)
+	//{
+	//	if(a > 100) break;
+	//	printf("a의 값은 %d입니다.\n",a);
+	//	a++; 
+	//}
+
+	// continue
+	//int a = 0;
+
+	//while(a < 100)
+	//{
+	//	a++;
+
+	//	if(a > 80 && a < 90) continue;
+	//	
+	//	printf("a의 값은 %d입니다. \n",a);
+	//}
+
+	// 2단출력
+	//int dan = 2;
+
+	//for(int i = 1; i < 10; i++) {
+	//	printf("%d * %d = %d\n", dan, i, dan * i);
+	//}
+
+	// 구구단 출력
+	for (int i = 2; i < 10; i++) {
+		printf("%d 단 입니다 ========= \n",i);
+		for(int j = 1; j < 10; j++) {
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
 	}
 }
